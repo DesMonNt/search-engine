ff193f7bf9d2f8563902e405478d5b76c9be740f
Grinik767 <grigorikiseleff@yandex.ru>
2024-09-03 23:24:56 +0500

    is_text_file func

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+import chardet
 
 
 class Utils:
@@ -17,3 +18,20 @@ class Utils:
             text = pattern.sub(lambda match: f'<span class="highlight">{match.group(0)}</span>', text)
 
         return text
+
+    @staticmethod
+    def is_text_file(file_path: str) -> bool:
+        try:
+            with open(file_path, 'rb') as file:
+                raw_data = file.read(1024)
+
+            encoding = chardet.detect(raw_data)['encoding']
+            if not encoding:
+                return False
+
+            with open(file_path, 'r', encoding=encoding) as file:
+                file.read(1024)
+
+            return True
+        except (UnicodeDecodeError, IOError):
+            return False
